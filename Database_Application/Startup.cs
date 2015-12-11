be0bd4aa7b70a6223e48903f89fbaabc8e9e976a
Jophy Johnson <johnson.jophy27@gmail.com>
2015-12-11 14:14:04 -0600

    initial commit

---
+++ Database_Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Database_Application.Startup))]
+namespace Database_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
