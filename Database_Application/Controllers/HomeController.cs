e0d960f1a01dccdd79a5e36001b13d5ef0fb3b46
Jophy Johnson <johnson.jophy27@gmail.com>
2015-12-11 17:38:23 -0600

    Site.css, homeController.cs, _viewStart.cshtml

--- Database_Application/Controllers/HomeController.cs
+++ Database_Application/Controllers/HomeController.cs
@@ -4,27 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Database_Application.Controllers
+namespace MvcDemo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
-        }
+        { return View(); }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        { return View(); }
     }
 }
